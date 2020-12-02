c015041ef2de12f5f83a8b25c78341685432043e
pobefer <pobefer@gmail.com>
2020-12-02 06:15:48 +0100

    s

--- Ejercicio07/Tarea6/Ejercicio7.js
+++ Ejercicio07/Tarea6/Ejercicio7.js
@@ -1,36 +1,36 @@
 var opciones;
-function init(){
+function init() {
     opciones = new Opciones();
 }
-class Opciones{
-    constructor(){
+class Opciones {
+    constructor() {
         $("#binaryP").hide();
         $("#SecuencialP").show();
     }
 
-    ocultar(){
+    ocultar() {
         $("#SecuencialP").hide();
         $("#binaryP").show();
         $("#TipoBusquedaH").text("Busqueda Binaria");
         $("#TipoBusquedaP").text("Codigo Binaria");
     }
 
-    mostrar(){
+    mostrar() {
         $("#SecuencialP").show();
         $("#binaryP").hide();
         $("#TipoBusquedaH").text("Busqueda Secuencial");
         $("#TipoBusquedaP").text("Codigo Secuencial");
     }
 
-    createElement(){
+    createElement() {
         var miParrafo = document.createElement("pre");
         var texto = document.getElementById("ta").value;
-        miParrafo.innerHTML = texto; 
-       
+        miParrafo.innerHTML = texto;
+
         $("#binaryP").after(miParrafo);
     }
 
-    deleteElement(){
+    deleteElement() {
         var p1 = document.getElementById("SecuencialP");
         var p2 = document.getElementById("binaryP");
 
@@ -38,19 +38,28 @@ class Opciones{
 
         $("#tipoBusquedaP").after(p2);
         $("#tipoBusquedaP").after(p1);
-       
+
 
         this.mostrar()
     }
 
-    viewElements(){
-        $("*", document.body).each(function() {
+    viewElements() {
+        $("*", document.body).each(function () {
             var etiquetaPadre = $(this).parent().get(0).tagName;
-            $(this).prepend(document.createTextNode( "Padre : <"  + etiquetaPadre + "> elemento : <" + $(this).get(0).tagName +"> valor: "));
+            $(this).prepend(document.createTextNode("Padre : <" + etiquetaPadre + "> elemento : <" + $(this).get(0).tagName + "> valor: "));
         });
     }
 
-    
+    sumarColumnas() {
+        var sum = 0;
+        $('.valor').each(function () {
+            sum += parseFloat($(this).text());
+        });
+        $('#resultado_total').empty();
+        $('#resultado_total').append("Suma: " + sum.toFixed(2));
+    }
+
+
 
 
 }
