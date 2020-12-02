2a59dafaf090cdf02bbaa10fbe2fe38a2162ed13
pobefer <pobefer@gmail.com>
2020-12-02 05:24:13 +0100

    s

--- Ejercicio10/Ejercicio10.js
+++ Ejercicio10/Ejercicio10.js
@@ -24,18 +24,16 @@ class Cotizaciones {
     $.ajax(settings).done(function (response) {
       var respuesta = response.marketSummaryResponse.result;
       for (var i = 0; i < respuesta.length; i++) {
-        $("#tabla").append("<tr><td id='exchange" + (i + 1) + "'></td><td id='TimeZone" + (i + 1) + "'></td><td id='Market" + (i + 1) + "'></td><td id='MarketChange" + (i + 1) + "'></td><td id='MarketChangePercentaje" + (i + 1) + "'></td><td id='ClosedAt" + (i + 1) + "'></td><td id='Price" + (i + 1) + "'></td><td id='Time" + (i + 1) + "'></td></tr>");
+        $("#tabla").append("<tr><td id='exchange" + (i + 1) + "'></td><td id='TimeZone" + (i + 1) + "'></td><td id='MarketChange" + (i + 1) + "'></td><td id='MarketChangePercentaje" + (i + 1) + "'></td><td id='ClosedAt" + (i + 1) + "'></td><td id='Price" + (i + 1) + "'></td></tr>");
 
       }
       for (var i = 0; i < respuesta.length; i++) {
         $("#exchange" + (i + 1)).append(respuesta[i].fullExchangeName);
         $("#TimeZone" + (i + 1)).append(respuesta[i].exchangeTimezoneShortName);
-        $("#Market" + (i + 1)).append(respuesta[i].market);
         $("#MarketChange" + (i + 1)).append(respuesta[i].regularMarketChange.fmt);
         $("#MarketChangePercentaje" + (i + 1)).append(respuesta[i].regularMarketChangePercent.fmt);
         $("#ClosedAt" + (i + 1)).append(respuesta[i].regularMarketPreviousClose.fmt);
         $("#Price" + (i + 1)).append(respuesta[i].regularMarketPrice.fmt);
-        $("#Time" + (i + 1)).append(respuesta[i].regularMarketTime.fmt);
       }
     });
   }
