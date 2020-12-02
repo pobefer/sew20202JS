668bf7ce0abc38dfe29a68432ea5ff50a086d4c7
pobefer <pobefer@gmail.com>
2020-12-02 05:11:34 +0100

    s

--- Ejercicio09/Ejercicio9.js
+++ Ejercicio09/Ejercicio9.js
@@ -21,11 +21,11 @@ class Meteo {
             success: function (datos) {
                 $("#ciudad" + (i+1)).append($('city',datos).attr("name"));
                 $("#icon" + (i+1)).attr("src", "https://openweathermap.org/img/w/" + $('weather',datos).attr("icon") + ".png");
-                $("#temperatura" + (i+1)).append($('temperature',datos).attr("value")+$('temperature',datos).attr("unit"));
-                $("#maxT" + (i+1)).append($('temperature',datos).attr("min")+$('temperature',datos).attr("unit"));
-                $("#minT" + (i+1)).append($('temperature',datos).attr("max")+$('temperature',datos).attr("unit"));
-                $("#presion" + (i+1)).append($('pressure',datos).attr("value")+$('pressure',datos).attr("unit"));
-                $("#humedad" + (i+1)).append($('humidity',datos).attr("value")+$('humidity',datos).attr("unit"));
+                $("#temperatura" + (i+1)).append($('temperature',datos).attr("value"));
+                $("#maxT" + (i+1)).append($('temperature',datos).attr("min"));
+                $("#minT" + (i+1)).append($('temperature',datos).attr("max"));
+                $("#presion" + (i+1)).append($('pressure',datos).attr("value"));
+                $("#humedad" + (i+1)).append($('humidity',datos).attr("value"));
             },
             error: function () {
                 document.write(this.error);
